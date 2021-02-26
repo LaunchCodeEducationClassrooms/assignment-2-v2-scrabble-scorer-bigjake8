b8e3fa21c3282b12737f60adfcc1b14f7ef4a9a6
Jacob Harris <jakedeckard@mac.com>
2021-02-26 20:56:14 +0000

    Did Part A. Just testing to make sure all is well.

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -33,8 +33,12 @@ function oldScrabbleScorer(word) {
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
-};
+
+console.log('Lets play some Scrabble!');
+console.log();
+word = input.question ("Enter a word to score: ");
+console.log(oldScrabbleScorer(word)); 
+}
 
 let simpleScore;
 
